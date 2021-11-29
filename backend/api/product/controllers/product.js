714085c04273e1972108459298a360645e0ab4fa
Sergio Junior Castro Egurrolar <sergiojuniorce@gmail.com>
2021-11-29 11:28:50 -0700

    added slug route for products

--- backend/api/product/controllers/product.js
+++ backend/api/product/controllers/product.js
@@ -1,8 +1,20 @@
 'use strict';
-
+const { sanitizeEntity } = require('strapi-utils');
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+ module.exports = {
+  /**
+   * Retrieve a record.
+   * 
+   * @return {Object}
+   */
+
+  async findOne(ctx) {
+    const { slug } = ctx.params;
+    const product = await strapi.services.category.findOne({ slug });
+    return sanitizeEntity(product, { model: strapi.models.product });
+  }
+}
